215d473a7704502c544f5f8af2f1574f8faeaa38
Protap Ghosh <protapg518@gmail.com>
2023-05-23 01:16:49 +0600

    add search field

--- src/pages/Home/Services/Services.jsx
+++ src/pages/Home/Services/Services.jsx
@@ -1,15 +1,22 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import ServiceCard from './ServiceCard';
 
 const Services = () => {
    const [services, setServices] = useState([]);
    const [ascending, setAscending] = useState([true]);
+   const searchRef = useRef(null);
+   const [search, setSearch] = useState('');
 
    useEffect(() => {
-      fetch(`http://localhost:5000/services?sort=${ascending ? 'ascending' : 'descending'}`)
+      fetch(`http://localhost:5000/services?search=${search}&sort=${ascending ? 'ascending' : 'descending'}`)
          .then((res) => res.json())
          .then((data) => setServices(data))
-   }, [ascending])
+   }, [ascending, search])
+
+   const handleSearch = () => {
+      // console.log(searchRef.current.value);
+      setSearch(searchRef.current.value)
+   }
 
    return (
       <div className='my-14'>
@@ -17,6 +24,17 @@ const Services = () => {
             <h3 className="text-[#FF3811] text-3xl font-semibold">Service</h3>
             <h2 className='text-5xl font-bold'>Our Service Area</h2>
             <p className='md:w-1/2 mx-auto capitalize'>the majority have suffered alteration in some form, by injected humour, or randomised words which do not look even slightly believable. </p>
+
+            <div className='flex justify-center'>
+               <div className="form-control">
+                  <div className="input-group">
+                     <input type="text" ref={searchRef} placeholder="Search…" className="input input-bordered w-96" />
+                     <button onClick={handleSearch} className="btn btn-square">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
+                     </button>
+                  </div>
+               </div>
+            </div>
 
             <button className='btn btn-outline text-[16px] capitalize' onClick={() => setAscending(!ascending)}>
                {ascending ? 'Price: High to Low' : 'Price: Low to High'}
