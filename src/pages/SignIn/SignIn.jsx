2abb69ade59181513d0c07fd22d76b8987066978
Protap Ghosh <protapg518@gmail.com>
2023-05-29 17:09:21 +0600

    veryify JWT Token

--- src/pages/SignIn/SignIn.jsx
+++ src/pages/SignIn/SignIn.jsx
@@ -1,23 +1,41 @@
-import React, { useContext } from 'react';
+import { useContext } from 'react';
 import Img from "../../assets/images/login/login.svg"
-import { Link } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 import { AuthContext } from '../../providers/AuthProvider';
 
 const SignIn = () => {
    const { signIn } = useContext(AuthContext);
+   const location = useLocation();
+   const navigate = useNavigate();
+   const from = location.state?.from?.pathname || '/';
 
    const handleSignIn = (event) => {
       event.preventDefault();
       const form = event.target;
       const email = form.email.value;
       const password = form.password.value;
-      const user = { email, password }
-      console.log(user);
 
       signIn(email, password)
          .then(result => {
             const user = result.user;
-            console.log(user);
+            const loggedUser = {
+               email: user.email,
+            }
+
+            fetch('http://localhost:5000/jwt', {
+               method: 'POST',
+               headers: {
+                  'content-type': 'application/json',
+               },
+               body: JSON.stringify(loggedUser)
+            })
+               .then((res) => res.json())
+               .then((data) => {
+                  // console.log('jwt response', data);
+                  // Warning: Local Storage is not the best solution ( second best place ) to store access token
+                  localStorage.setItem('car-access-token', data.token);
+                  navigate(from, { replace: true })
+               })
          })
          .catch(error => {
             console.log(error);
--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -13,12 +13,12 @@ const SignUp = () => {
       const email = form.email.value;
       const password = form.password.value;
       const user = { name, email, password }
-      console.log(user);
+      // console.log(user);
 
       createUser(email, password)
          .then(result => {
             const user = result.user;
-            console.log(user);
+            // console.log(user);
          })
          .catch(error => {
             console.log(error);
